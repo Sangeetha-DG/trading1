218241a8c35bd0473c3d221451960d4f1024e225
Sangeetha-DG <sangeethagopalrao@gmail.com>
2025-08-29 08:59:38 -0400

    Crypto APP first look

--- nodepm/hello-app/app/page.tsx
+++ nodepm/hello-app/app/page.tsx
@@ -1,5 +1,14 @@
+import CryptoChart from "@/components/crypto_d";
+
 export default function Home() {
   return (
-    <h1>Hello World 🚀</h1>
-  )
+    <main className="min-h-screen bg-gray-50 py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
+          Crypto Details
+        </h1>
+        <CryptoChart />
+      </div>
+    </main>
+  );
 }
